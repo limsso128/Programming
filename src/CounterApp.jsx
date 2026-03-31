6a7ce5a3a83f321dab6ca51dce42f44dee67ce21
limsso128 <s2412@e-mirim.hs.kr>
2026-03-31 09:45:40 +0900

    CounterApp Too many re-renders(강조!!) 😗

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -14,7 +14,9 @@ function CounterApp() {
     <button onClick={() => setCount((count) => count + 1)}>+</button>
     <button onClick={() => setCount((count) => count + 2)}>+2</button>
     <button onClick={() => setCount((count) => count - 1)}>-</button>
-    
+    <button onClick={() => setCount(0)}>reset</button>
+    {/* <button onClick={setCount(0)}>reset</button> //Too many re-renders 에러 */}
+
     {/* <button
       className="counter"
       onClick={() => setCount((count) => count + 1)} //JS 함수호출X, 함수정의O
